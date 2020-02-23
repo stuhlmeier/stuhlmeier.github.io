31203a75c75cdf30aedea85903555139aae902eb
stuhlmeier <stuhlmeier@gmail.com>
2020-02-23 13:18:01 +0100

    Add animated page subheader

---
+++ static/js/data.js
@@ -0,0 +1,30 @@
+const descriptions = Object.freeze([
+    "software developer",
+    "web designer",
+    "open-source lover",
+    "Linux enthusiast",
+    "Kotlin expert",
+]);
+
+function getRandom(array, count) {
+    function shuffleArray(array) {
+        const copy = [...array];
+        for (let i = copy.length - 1; i > 0; --i) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [copy[i], copy[j]] = [copy[j], copy[i]];
+        }
+        return copy;
+    }
+
+    return shuffleArray(array).slice(0, count);
+}
+
+const shuffle = [
+    ...getRandom(descriptions.slice(1), 3),
+    descriptions[0]
+];
+
+function getInitialDescription() {
+    console.log(shuffle);
+    return shuffle[0];
+}
